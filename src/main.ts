0b26e71b90b7863b841883a56307c75c5d43615d
tiagoolima <tiagotome097@gmail.com>
2024-09-23 18:56:05 -0300

    Adiciona um placeholder aos inputs

--- src/main.ts
+++ src/main.ts
@@ -5,25 +5,25 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <h1>Calculadora</h1>
     <h2>Adição:</h2>
     <label>
-      <input id="addNumber1"> + <input id="addNumber2">
+      <input placeholder="Digite um número" id="addNumber1"> + <input placeholder="Digite um número" id="addNumber2">
       <Button id="calcAdd">Calcular</Button>
     </label>
 
     <h2>Subtração:</h2>
     <label>
-      <input id="subNumber1"> - <input id="subNumber2">
+      <input placeholder="Digite um número" id="subNumber1"> - <input placeholder="Digite um número" id="subNumber2">
       <Button id="calcSub">Calcular</Button>
     </label>
 
     <h2>Multiplicação:</h2>
     <label>
-      <input id="multNumber1"> x <input id="multNumber2">
+      <input placeholder="Digite um número" id="multNumber1"> x <input placeholder="Digite um número" id="multNumber2">
       <Button id="calcMult">Calcular</Button>
     </label>
 
     <h2>Divisão:</h2>
     <label>
-      <input id="divNumber1"> : <input id="divNumber2">
+      <input placeholder="Digite um número" id="divNumber1"> : <input placeholder="Digite um número" id="divNumber2">
       <Button id="calcDiv">Calcular</Button>
     </label>
       
